be9f145b188f7014958d81361bc0029570bcb7b0
rafaelrocha <rafaelmachadodarocha@gmail.com>
2020-09-24 00:09:44 -0300

    implementing controlled component

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'; 
 
 class SearchBar extends Component {
+
+  constructor() {
+    super(props)
+    this.state = {
+      term: ""
+    }
+  }
   handleUpdate = (event) => {
+    this.setState({term: event.target.value})
     this.props.search(event.target.value);
   }
   render () {
-    return <input type="text" className="form-control form-search" onChange={this.handleUpdate}/>
+    return <input value={this.state.term} type="text" className="form-control form-search" onChange={this.handleUpdate}/>
   }
 }
 
